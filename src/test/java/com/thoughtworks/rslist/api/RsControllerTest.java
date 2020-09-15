d4b60952819efc19bf8a54c779be93b0289e8201
CH <1925480119@qq.com>
2020-09-15 21:59:30 +0800

    [ChenHui] getRsEventList

---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_get_rs_event_list() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+    }
+
+
+
+}
