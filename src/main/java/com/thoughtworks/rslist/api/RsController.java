09926774ae4102f09f04fd26b876b9ef30b524cd
CH <1925480119@qq.com>
2020-09-15 21:11:48 +0800

    [chenhui]getRsEventList

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,17 +1,54 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.domain.RsEvent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("routing", "data-binding", "validation","customize-response","error-handling");
+//  private List<RsEvent> rsList = initReEventList();
+private List<String> rsList = Arrays.asList("aaaa","bbbb","cccc");
 
-  @GetMapping("/rs/list")
-  public String getrsList(){
-    return rsList.toString();
-  }
+//  private List<RsEvent> initReEventList() {
+//    List<RsEvent> rsEventList = new ArrayList<>();
+//    rsEventList.add(new RsEvent("第一事件","无关键"));
+//    rsEventList.add(new RsEvent("第二事件","无关键"));
+//    rsEventList.add(new RsEvent("第三事件","无关键"));
+//    rsEventList.add(new RsEvent("第四事件","无关键"));
+//
+//    return rsEventList;
+//  }
+
+
+
+//  @GetMapping("/rs/list")
+//  public List<RsEvent> getrsList(){
+//    return rsList;
+//  }
+//    @GetMapping("/rs/list")
+//    public String getRsEvent(){
+//      return rsList.toString();
+//    }
+
+    @GetMapping("/rs/{index}")
+    public String getOneRsEvent(@PathVariable int index){
+        return rsList.get(index-1);
+    }
+
+    @GetMapping("/rs/list")
+    public String getRsEvent(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end){
+        if(start == null || end == null){
+            return rsList.toString();
+        }
+        return rsList.subList(start-1, end).toString();
+    }
+
+
+
 }
--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,11 +19,31 @@ class RsControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+//
+//    @Test
+//    public void should_return_asList() throws Exception {
+//        mockMvc.perform(get("/rs/list"))
+//                .andExpect((ResultMatcher) jsonPath("$",hasSize(4)))
+//                .andExpect(status().isOk());
+//    }
+    @Test
+    public void should_get_rs_event_list() throws  Exception{
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(content().string("[aaaa, bbbb, cccc]"))
+                .andExpect(status().isOk());
+    }
 
     @Test
-    public void should_return_asList() throws Exception {
-        mockMvc.perform(get("/rs/list"))
-                .andExpect(content().string("[routing, data-binding, validation, customize-response, error-handling]"))
+    public void should_get_one_rs_event_list() throws Exception {
+        mockMvc.perform(get("/rs/1"))
+                .andExpect(content().string("aaaa"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_get_some_rs_event_list() throws Exception {
+        mockMvc.perform(get("/rs/list?start=1&end=2"))
+                .andExpect(content().string("[aaaa, bbbb]"))
                 .andExpect(status().isOk());
     }
 
