0a6d0dcfa1b4799e9025b40549804a76bb91e59d
CH <1925480119@qq.com>
2020-09-15 15:04:53 +0800

    [chenhui] implement return asList

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,19 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
 
 @SpringBootTest
 class RsListApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+
     }
 
 }
